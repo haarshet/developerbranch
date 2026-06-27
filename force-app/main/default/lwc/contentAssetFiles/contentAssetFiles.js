a94e5155527b1f8152c0649e9ba2d74b6a0593c4
haarshet <hashethari.28@gmail.com>
2026-06-27 10:20:11 +0530

    Content aseet file - 81

---
+++ force-app/main/default/lwc/contentAssetFiles/contentAssetFiles.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+import JS_FILE from '@salesforce/contentAssetUrl/JavaScriptLWCSalesforcepptx';
+
+export default class ContentAssetFiles extends LightningElement {
+
+    file = JS_FILE;
+
+
+}
