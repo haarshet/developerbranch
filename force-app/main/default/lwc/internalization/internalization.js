40f9bb425b564591c09ae8207e245cf11cda1516
haarshet <hashethari.28@gmail.com>
2026-06-27 14:36:00 +0530

    Internalization currency based on locale- 82

---
+++ force-app/main/default/lwc/internalization/internalization.js
@@ -0,0 +1,15 @@
+import { LightningElement } from 'lwc';
+import LOCALE from '@salesforce/i18n/locale';
+import CURRENCY from '@salesforce/i18n/currency';
+
+export default class Internalization extends LightningElement {
+
+    number = 345679;
+    formattedNumber = new Intl.NumberFormat(LOCALE,{
+        style:'currency',
+        currency:CURRENCY,
+        currencyDisplay:'symbol'
+    }).format(this.number)
+
+
+}
